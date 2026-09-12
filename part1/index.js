0f825b35a868cdece0fb52a6e538eca004686b3c
MuhammadHasaan546-eng <muhammadhasaanm546@gmail.com>
2026-09-12 17:16:05 +0500

    feat: update AI model and add dependencies for generative AI

--- part1/index.js
+++ part1/index.js
@@ -1,20 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
-
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 dotenv.config();
 
-
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-3.6-flash",
+  temperature: 0,
+  maxRetries: 2,
+});
 
-const llm = new ChatGoogleGenerativeAI ({
-      model: "gemini-2.5-pro",
-    temperature: 0,
-    maxRetries: 2,
-})
+app.post("/ai", async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { input } = req.body;
+    const response = await llm.invoke(input);
+
+    return res.status(200).json({ ai: response.content });
+  } catch (error) {
+    console.error("AI Error:", error);
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
